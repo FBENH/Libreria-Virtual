9038265efb28e4e31fdeac9142a2715667321df4
FBENH <federicob1399@gmail.com>
2025-03-27 03:04:11 -0300

    Se agregan DTO y clase de Respuesta Se agrega DTO para no exponer las entidades de db. Se agrega una clase de respuesta para devolver en las requests.

---
+++ App/API/Models/DTO/UsuarioRegistroDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.DTO
+{
+    public class UsuarioRegistroDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string? UrlFoto { get; set; }
+    }
+}
---
+++ App/API/Models/Respuesta/Respuesta.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Models.Respuesta
+{
+    public class Respuesta
+    {
+        public int exito { get; set; }
+
+        public string? mensaje { get; set; }
+
+        public object? data { get; set; }
+
+        public Respuesta()
+        {
+            exito = 0;
+        }
+    }
+}
